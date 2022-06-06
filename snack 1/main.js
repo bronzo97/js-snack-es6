a988c30468b7ead837e319bd2e15425945c89264
bronzo97 <danilobronzo97@hotmail.it>
2022-06-06 15:57:11 +0200

    first Snack done

---
+++ snack 1/main.js
@@ -0,0 +1,48 @@
+const bikes = [
+    {
+        bikeName: 'Street Power',
+        bikeWeightGrams: 7000,
+    },
+    {
+        bikeName: 'Speed',
+        bikeWeightGrams: 5000,
+    },
+    {
+        bikeName: 'Montain',
+        bikeWeightGrams: 7500,
+    },
+    {
+        bikeName: 'Hibrid',
+        bikeWeightGrams: 7000,
+    },
+    {
+        bikeName: 'Electric MDG',
+        bikeWeightGrams: 9000,
+    },
+    {
+        bikeName: 'City View',
+        bikeWeightGrams: 8500,
+    }
+];
+
+const {bikeName, bikeWeightGrams} = bikes[0];
+
+
+let lighterBike = {
+    bikeName,
+    bikeWeightGrams
+};
+
+for (let i = 0; i < bikes.length; i++) {
+const {bikeName, bikeWeightGrams} = bikes[i];
+
+if (lighterBike.bikeWeightGrams > bikeWeightGrams) {
+    lighterBike = {
+        bikeName,
+        bikeWeightGrams
+    }
+}
+    
+}
+
+console.log(lighterBike);
