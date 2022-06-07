563815ab0f1d28b462197c72b49f2c6cb164a663
bronzo97 <danilobronzo97@hotmail.it>
2022-06-07 17:05:35 +0200

    ...

---
+++ snack 3/main.js
@@ -0,0 +1,16 @@
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+let min = 2;
+let max = 5;
+
+myArray.forEach(element => {
+        const filtered = myArray.filter() 
+            if (i > min && i < max) {
+                return userName;
+            };
+        });
+            
+        // });
+console.log(filtered);
+
+
+// const numeriDispari = numeri.filter((numero) => numero % 2 === 1 && numero > 5);
---
+++ snack 4/main.js
@@ -0,0 +1,13 @@
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luig', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+    ];
+
+    students.forEach(element => {
+        console.log(element.name.toLocaleUpperCase());
+    });
